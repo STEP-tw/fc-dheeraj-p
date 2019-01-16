9bf986b8adc57832c3f3c70c0d9415c5b01f886c
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-01-16 12:23:09 +0530

    Serve HomePage with basic styling
    Server can server almost all type of file requested by
    client if available

--- src/app.js
+++ src/app.js
@@ -1,8 +1,31 @@
-const app = (req, res) => {
-  res.statusCode = 404;
-  res.end();
+const fs = require('fs');
+const REDIRECTS = { './': './index.html' };
+
+const resolveRequestedFile = function(url) {
+  let requestedFile = `.${url}`;
+  return REDIRECTS[requestedFile] || requestedFile;
 };
 
+const app = (req, res) => {
+  const requestedFile = resolveRequestedFile(req.url);
+  fs.readFile(requestedFile, (err, content) => {
+    if (!err) {
+      send(res, 200, content);
+      return;
+    }
+    if (err.code == 'ENOENT') {
+      send(res, 404, '404: File Not Found');
+      return;
+    }
+    send(res, 500, '500: Internal Server Error');
+  });
+};
+
+const send = function(res, statusCode, content) {
+  res.statusCode = statusCode;
+  res.write(content);
+  res.end();
+};
 // Export a function that can act as a handler
 
 module.exports = app;
