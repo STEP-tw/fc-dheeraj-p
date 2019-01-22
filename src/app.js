2e8eaec9f3baf6f0348d33730565d7effdbc14a4
Dheeraj Pundir <44019618+dheeraj-p@users.noreply.github.com>
2019-01-22 22:36:13 +0530

    Login feature implemented partialy.
    User can login using their username.
    Server will remember the user once logged in but lose information if
    server dies.
    Logging out is not implemented yet.

--- src/app.js
+++ src/app.js
@@ -7,7 +7,11 @@ const {
   UTF8_ENCODING,
   REDIRECTS,
   MIME_TYPES,
-  MIME_TEXT_PLAIN
+  MIME_TEXT_PLAIN,
+  LOGIN_FORM_TEMPLATE_PATH,
+  COMMENT_FORM_TEMPLATE_PATH,
+  FORM_PLACEHOLDER,
+  GUEST_BOOK_PAGE_TEMPLATE_PATH
 } = require('./constants');
 
 const app = new Sheeghra();
@@ -73,13 +77,13 @@ const logRequests = function(req, res, next) {
   next();
 };
 
-const saveComment = function(comment, res, next) {
+const saveComment = function(req, res, comment) {
   comments.unshift(comment);
   fs.writeFile(COMMENTS_FILE, JSON.stringify(comments), err => {
     if (err) {
       return send(res, 500, ERROR_500);
     }
-    next();
+    serveGuestBookPage(req, res);
   });
 };
 
@@ -109,17 +113,75 @@ const readPostBody = (req, res, next) => {
 const postComment = function(req, res, next) {
   const comment = readParameters(req.body);
   comment.date = new Date();
-  saveComment(comment, res, next);
+  saveComment(req, res, comment);
 };
 
 const serveComments = function(req, res) {
   send(res, 200, JSON.stringify(comments), resolveMIMEType('json'));
 };
 
+const loggedInUsers = [];
+
+const readCookies = function(cookieHeader) {
+  return cookieHeader
+    .split(';')
+    .map(splitKeyValue)
+    .reduce(assignKeyValue, {});
+};
+
+const isUserLoggedIn = function(req) {
+  const cookieHeader = req.headers.cookie;
+  if (!cookieHeader) {
+    return false;
+  }
+  const cookies = readCookies(cookieHeader);
+  return loggedInUsers.includes(cookies.username);
+};
+
+const LOGIN_FORM_HTML = fs.readFileSync(
+  LOGIN_FORM_TEMPLATE_PATH,
+  UTF8_ENCODING
+);
+const COMMENT_FORM_HTML = fs.readFileSync(
+  COMMENT_FORM_TEMPLATE_PATH,
+  UTF8_ENCODING
+);
+const GUEST_BOOK_TEMPLATE = fs.readFileSync(
+  GUEST_BOOK_PAGE_TEMPLATE_PATH,
+  UTF8_ENCODING
+);
+
+const getCommentsFormPage = function() {
+  return GUEST_BOOK_TEMPLATE.replace(FORM_PLACEHOLDER, COMMENT_FORM_HTML);
+};
+
+const getLoginFormPage = function() {
+  return GUEST_BOOK_TEMPLATE.replace(FORM_PLACEHOLDER, LOGIN_FORM_HTML);
+};
+
+const serveGuestBookPage = function(req, res, next) {
+  let guestBookPageHTML = getLoginFormPage();
+  if (isUserLoggedIn(req)) {
+    guestBookPageHTML = getCommentsFormPage();
+  }
+  send(res, 200, guestBookPageHTML, resolveMIMEType('html'));
+};
+
+const doLogin = function(req, res) {
+  const { username } = readParameters(req.body);
+  loggedInUsers.push(username);
+  res.setHeader('Set-Cookie', `username=${username}`);
+  res.statusCode = 302;
+  res.setHeader('location', '/guest_book.html');
+  res.end();
+};
+
 app.use(logRequests);
 app.use(readPostBody);
 app.get('/comments', serveComments);
+app.get('/guest_book.html', serveGuestBookPage);
 app.post('/guest_book.html', postComment);
+app.post('/login', doLogin);
 app.use(serveFile);
 
 // Export a function that can act as a handler
--- src/constants.js
+++ src/constants.js
@@ -3,6 +3,10 @@ const ERROR_500 = '500: Internal Server Error';
 const COMMENTS_FILE = './private/comments.json';
 const UTF8_ENCODING = 'utf-8';
 const REDIRECTS = { './public_html/': './public_html/index.html' };
+const LOGIN_FORM_TEMPLATE_PATH = './private/login_form.template.html';
+const COMMENT_FORM_TEMPLATE_PATH = './private/comment_form.template.html';
+const GUEST_BOOK_PAGE_TEMPLATE_PATH = './private/guest_book.template.html';
+const FORM_PLACEHOLDER = '___FORM_GOES_HERE___';
 const MIME_TEXT_PLAIN = 'text/plain';
 const MIME_TYPES = {
   css: 'text/css',
@@ -27,5 +31,9 @@ module.exports = {
   UTF8_ENCODING,
   REDIRECTS,
   MIME_TYPES,
-  MIME_TEXT_PLAIN
+  MIME_TEXT_PLAIN,
+  LOGIN_FORM_TEMPLATE_PATH,
+  COMMENT_FORM_TEMPLATE_PATH,
+  GUEST_BOOK_PAGE_TEMPLATE_PATH,
+  FORM_PLACEHOLDER
 };
